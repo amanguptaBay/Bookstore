59aafeb9c8d6536c547b2eb61ee9eed9f7597094
Aman <47506369+amanguptaBay@users.noreply.github.com>
2024-01-03 20:16:06 -0800

    Hello World

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,18 @@
 import express from "express";
 import {PORT} from "./config.js"
 const app = express();
+
+app.get("/", (request, response) => {
+    let statusCode = 404;
+    let body = "";
+
+    statusCode = 200;
+    body = "Hello World";
+
+    return response.status(statusCode).send(body);
+    
+})
+
 app.listen(PORT, () => {
     console.log(`Server has started on port: ${PORT}`);
 })
