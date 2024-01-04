f7d4c29b6dd48abe0e3e4ffd76ea69fc1b93ef38
Aman <47506369+amanguptaBay@users.noreply.github.com>
2024-01-03 20:51:17 -0800

    Book Schema + Post API

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,4 @@
 export const PORT = 5555;
+const USERNAME = "root";
+const PASSWORD = "Alpharetta_30076"
+export const mongoURL = `mongodb+srv://${USERNAME}:${PASSWORD}@bookstore-mern.ohqurin.mongodb.net/books-collection?retryWrites=true&w=majority`;
--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,56 @@
 import express from "express";
-import {PORT} from "./config.js"
+import {PORT, mongoURL} from "./config.js"
+import mongoose from "mongoose";
+import { Book } from "./models/bookSchema.js";
+
 const app = express();
+app.use(express.json())
+
+
+app.post("/books", async (request, response) => {
+    try{
+       if(!(request.body.author && request.body.title && request.body.publishYear)){
+        response.status(400).send({
+            message: "Missing One or More Required Fields: author, title, publishYear"
+        })
+       }
+
+       const book = await Book.create({
+            title: request.body.title,
+            author: request.body.author,
+            publishYear: request.body.publishYear
+       })
+
+       response.status(201).send(book);
+      
+
+    } catch (error) {
+        console.log(error.message)
+        response.status(500).send({message: error.message})
+    }
+})
 
 app.get("/", (request, response) => {
     let statusCode = 404;
     let body = "";
 
-    statusCode = 200;
-    body = "Hello World";
+    statusCode = 404;
+    body = "Requested Resource does not exist on server";
 
     return response.status(statusCode).send(body);
     
 })
 
-app.listen(PORT, () => {
-    console.log(`Server has started on port: ${PORT}`);
+console.log("Initializing App")
+
+mongoose
+.connect(mongoURL)
+.then(() => {
+    console.log("Connected to DB")
+    app.listen(PORT, () => {
+        console.log(`Server has started on port: ${PORT}`);
+    })
+})
+.catch((error) => {
+    console.log(error)
 })
