39334fd2e46ac56cf6371fbf7efeefd0fcb3d5af
Aman <47506369+amanguptaBay@users.noreply.github.com>
2024-01-04 18:48:16 -0800

    Added cors total allow permission

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,11 @@ import express from "express";
 import {PORT, mongoURL} from "./config.js"
 import mongoose from "mongoose";
 import booksRoute from "./routes/booksRoute.js"
+import cors from "cors"
+
 const app = express();
 app.use(express.json())
+app.use(cors())
 
 app.use("/books", booksRoute)
 
