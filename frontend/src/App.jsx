780dacac1689b782ba87a157b7a40edce1c7dced
Aman <47506369+amanguptaBay@users.noreply.github.com>
2024-01-06 20:25:50 -0800

    Booklist rendering

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import {CreateBooks} from './pages/CreateBooks.jsx'
+import {DeleteBook} from './pages/DeleteBook.jsx'
+import {EditBook} from './pages/EditBook.jsx'
+import {Home} from './pages/Home.jsx'
+import {ShowBook} from './pages/ShowBook.jsx'
 
 const App = () => {
   return (
-      <div className='bg-red-400 text-white'>App</div>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/books/create' element={<CreateBooks />} />
+        <Route path='/books/details/:id' element={<ShowBook />} />
+        <Route path='/books/edit/:id' element={<EditBook />} />
+        <Route path='/books/delete/:id' element={<DeleteBook />} />
+      </Routes>
   )
 }
 
