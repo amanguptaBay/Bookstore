a5af0187f020b4f3d26d4a3760c76fbe7430b20a
Aman <47506369+amanguptaBay@users.noreply.github.com>
2024-01-10 10:37:38 -0800

    Added modal to look at the book,

--- frontend/src/components/home/BookInteraction.jsx
+++ frontend/src/components/home/BookInteraction.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
+import {useState} from 'react'
 import { Link } from 'react-router-dom'
 import {AiOutlineEdit} from 'react-icons/ai'
+import {BiShow} from 'react-icons/bi'
 import {BsInfoCircle} from 'react-icons/bs'
 import {MdOutlineAddBox, MdOutlineDelete} from 'react-icons/md'
+import { BookModal } from './BookModal'
 
 export const BookInteraction = ({book}) => {
-  return (
+    const [showModal, setShowModal] = useState(false);
+    return (
     <div className='flex justify-center gap-x-4'>
+    <BiShow className='text-3xl text-blue-800 hover:text-black cursor-pointer' onClick={() => setShowModal(true)}/>
     <Link to={`/books/details/${book._id}`}>
         <BsInfoCircle className='text-2xl text-green-800' />
     </Link>
@@ -16,6 +20,9 @@ export const BookInteraction = ({book}) => {
     <Link to={`/books/delete/${book._id}`}>
         <MdOutlineDelete className='text-2xl text-red-600 hover:text-black' />
     </Link>
+    {
+        showModal ? <BookModal book={book} onClose={()=>setShowModal(false)}/> : <div></div>
+    }
     </div>
-  )
+    )
 }
